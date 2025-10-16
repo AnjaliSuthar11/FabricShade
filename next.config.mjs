bf0e8d200cb13f5c6649b6dd77f2e52953b98c79
Anjali Suthar <anjalisuthar114@gmail.com>
2025-10-16 09:41:13 +0530

    next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+     images: {
+    unoptimized: true, // disables Image Optimization globally
+  },
+};
 
 export default nextConfig;
