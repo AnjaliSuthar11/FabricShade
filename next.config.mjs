245766a98f624ae7930a1c916c6c826c29bd61e9
Anjali Suthar <anjalisuthar114@gmail.com>
2025-10-16 09:47:54 +0530

    include lampshade images

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-     images: {
-    unoptimized: true, // disables Image Optimization globally
-  },
+    
 };
 
 export default nextConfig;
